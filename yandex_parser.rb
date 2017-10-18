cb1ef1a9a55e247d14b1a89d2c34eb4323087f93
Tilvin <kate2homy@gmail.com>
2017-10-18 21:40:45 +0300

    add yandex_rank

--- yandex_parser.rb
+++ yandex_parser.rb
@@ -21,18 +21,27 @@ class YandexParser
 
     @data = []
     @page.css(".serp-item.t-construct-adapter__legacy .link.link_theme_normal.organic__url").each do |link_element|
-      uri_alexa = URI.parse("https://www.alexa.com/siteinfo/#{link_element['href']}")
-      @page_alexa = Nokogiri::HTML(open(uri_alexa.to_s))
-      rank = @page_alexa.css(".globleRank .col-pad .metrics-data").text.gsub(/\n\n/,'')
-      @data << [link_element['href'], rank.to_i]
+
+      link = link_element['href'].gsub(/(http|https):\/\//, '')
+
+      uri_alexa = URI.parse("https://www.alexa.com/siteinfo/#{ link }")
+      page_alexa = Nokogiri::HTML(open(uri_alexa.to_s))
+      alexa_rank = page_alexa.css(".globleRank .col-pad .metrics-data").text.gsub(/\n\n/,'')
+
+
+      uri_tiz = URI.parse("https://yandex.ru/yaca/cy/ch/#{ link }")
+      page_tiz = Nokogiri::HTML(open(uri_tiz.to_s))
+      tiz = page_tiz.css('.cy__not-described-cy').text.match(/\d+/).to_s
+
+      @data << [link_element['href'], alexa_rank.to_i, tiz]
     end
   end
 
   def create_csv
     CSV.open("rails_file.csv", "wb") do |csv|
-      csv << ["ADDRESS", "ALEXA GLOBAL RATE"]
-      @data.sort_by(&:last).reverse.each do |address, rate|
-        csv << [address, rate]
+      csv << ["ADDRESS", "ALEXA GLOBAL RATE", "YANDEX RATE"]
+      @data.sort_by{ |x,y,z| - y }.each do |address, alexa_rate, yandex_rate|
+        csv << [address, alexa_rate, yandex_rate]
       end
     end
   end
